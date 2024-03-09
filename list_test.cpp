44dcb5d65eef7fc6092f1dd92381b3beb9afb739
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-03-09 14:09:24 -0600

    Add code to capture stdout from functions and test it

--- list_test.cpp
+++ list_test.cpp
@@ -1,9 +1,36 @@
 #define CATCH_CONFIG_MAIN
 
+#include <iostream>
+#include <sstream>
+
 #include "catch_amalgamated.hpp"
 #include "list.hpp"
 
-TEST_CASE("1: List Node Value Test", "[list]"){
-    ListNode myListNode1(1);
-    REQUIRE(myListNode1.val == 1);
+
+void foo(){
+    std::cout << "foo" << std::endl;
+}
+
+class AutoRestoreRdbuf {
+    std::ostream& out;
+    std::streambuf* old;
+
+    public:
+        ~AutoRestoreRdbuf(){ out.rdbuf(old); }
+        AutoRestoreRdbuf(const AutoRestoreRdbuf&) = delete;
+        AutoRestoreRdbuf(AutoRestoreRdbuf&&) = delete;
+        AutoRestoreRdbuf(std::ostream& out) : out { out }, old { out.rdbuf() } {}
+};
+
+std::string stringWrittentToStream(std::function<void()> myFunction, std::ostream& out = std::cout){
+    AutoRestoreRdbuf restore { out };
+    std::ostringstream oss;
+    std::cout.rdbuf(oss.rdbuf());
+    myFunction();
+    return oss.str();
+}
+
+TEST_CASE("0: Foo", "testtest"){
+    auto s = stringWrittentToStream(&foo);
+    REQUIRE(s == "foo\n");
 }
