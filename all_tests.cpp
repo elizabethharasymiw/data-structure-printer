af7a431cf65ccf4f16e5221fa3c3126d77dbc181
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-03-15 22:54:23 -0500

    Coverted manual tree printing tests into catch2 unit tests

--- all_tests.cpp
+++ all_tests.cpp
@@ -7,6 +7,7 @@
 #include "catch_amalgamated.hpp"
 #include "list.hpp"
 #include "test_support.hpp"
+#include "tree.hpp"
 
 // Testing function that just prints "foo" and a newline to the stdout
 void printFoo() { std::cout << "foo" << std::endl; }
@@ -41,5 +42,36 @@ TEST_CASE("2: Linked List Print Tests", "[list]") {
         std::function<void()> func = std::bind(printLinkedList, revList);
         auto s = stringWrittentToStream(func, std::cout);
         REQUIRE(s == "(3)->(2)->(1)->nullptr\n");
+    }
+}
+
+TEST_CASE("3: Tree Print Tests", "[tree]") {
+
+    // InOrder example of Tree in an Array
+    // [3, 2, 4, 1,null,null, 5]
+
+    // Manuel build example tree for testing
+    TreeNode myTreeRoot(3);
+    TreeNode myTree2(2);
+    TreeNode myTree1(1);
+    TreeNode myTree4(4);
+    TreeNode myTree5(5);
+
+    myTreeRoot.left = &myTree2;
+    myTreeRoot.right = &myTree4;
+
+    myTree2.left = &myTree1;
+    myTree4.right = &myTree5;
+
+    SECTION("A: Print DFT of Tree") {
+        std::function<void()> func = std::bind(printDFTPath, &myTreeRoot);
+        auto s = stringWrittentToStream(func, std::cout);
+        REQUIRE(s == "3\n2\n1\n4\n5\n");
+    }
+
+    SECTION("B: Print in order traversal of Tree ") {
+        std::function<void()> func = std::bind(printInOrderPath, &myTreeRoot);
+        auto s = stringWrittentToStream(func, std::cout);
+        REQUIRE(s == "1\n2\n3\n4\n5\n");
     }
 }
--- main.cpp
+++ main.cpp
@@ -3,29 +3,4 @@
 #include "list.hpp"
 #include "tree.hpp"
 
-int main(int argc, char *argv[]) {
-
-    // InOrder example of Tree in an Array
-    // [3, 2, 4, 1,null,null, 5]
-
-    // Manuel build example tree for testing
-    TreeNode myTreeRoot(3);
-    TreeNode myTree2(2);
-    TreeNode myTree1(1);
-    TreeNode myTree4(4);
-    TreeNode myTree5(5);
-
-    myTreeRoot.left = &myTree2;
-    myTreeRoot.right = &myTree4;
-
-    myTree2.left = &myTree1;
-    myTree4.right = &myTree5;
-
-    std::cout << "printing DFT of Tree Example" << std::endl;
-    printDFTPath(&myTreeRoot);
-
-    std::cout << "printing in order traversal of Tree Example" << std::endl;
-    printInOrderPath(&myTreeRoot);
-
-    return 0;
-}
+int main(int argc, char *argv[]) { return 0; }
